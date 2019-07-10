bf689dba24831933f4edfef01b051901cb5a4f4e
imnotsahil <47858118+imnotsahil@users.noreply.github.com>
2019-07-10 18:20:50 +0530

    update dbconnect.php

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 	//make connection with database
-	$link = mysqli_connect("db4free.net","sahil12","password","admin12");
+	$link = mysqli_connect("db4free.net","sahil12","password","credit12");
 	// Check connection
 	if($link === false) {
     	die("ERROR: Could not connect. " . mysqli_connect_error());
